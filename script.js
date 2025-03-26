e6a67d0f1a65a4e2cd8a98effd45597760da938e
Andy930809 <2380267646@qq.com>
2025-03-26 21:37:45 +0800

    增加图片以及视频

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+
 document.addEventListener("DOMContentLoaded", function () {
     console.log("页面加载完成！");
+
+    // 图片放大预览
+    const galleryImages = document.querySelectorAll(".gallery img");
+    const lightbox = document.getElementById("lightbox");
+    const lightboxImg = document.getElementById("lightbox-img");
+
+    galleryImages.forEach(img => {
+        img.addEventListener("click", function () {
+            lightboxImg.src = this.src;
+            lightbox.classList.remove("hidden");
+        });
+    });
+
+    lightbox.addEventListener("click", function () {
+        lightbox.classList.add("hidden");
+    });
 });
